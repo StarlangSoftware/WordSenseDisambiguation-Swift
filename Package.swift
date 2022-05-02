30435f96a9c20ef43d914e3a425432952a93052a
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-02 09:03:39 +0300

    Updated AnnotatedTree dependency.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["WordSenseDisambiguation"]),
     ],
     dependencies: [
-        .package(name: "AnnotatedTree", url: "https://github.com/StarlangSoftware/AnnotatedTree-Swift.git", .exact("1.0.4")),
+        .package(name: "AnnotatedTree", url: "https://github.com/StarlangSoftware/AnnotatedTree-Swift.git", .exact("1.0.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
