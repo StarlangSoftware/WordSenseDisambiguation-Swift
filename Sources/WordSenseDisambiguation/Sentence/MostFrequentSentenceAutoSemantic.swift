34177422908c33257904e3190b9dcb572deaeb2b
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-02-20 22:17:33 +0300

    Updated MostFrequentAutoSemantic.

--- Sources/WordSenseDisambiguation/Sentence/MostFrequentSentenceAutoSemantic.swift
+++ Sources/WordSenseDisambiguation/Sentence/MostFrequentSentenceAutoSemantic.swift
@@ -28,37 +28,109 @@ public class MostFrequentSentenceAutoSemantic : SentenceAutoSemantic{
         self.fsm = fsm
     }
 
-    private func mostFrequent(synSets: [SynSet], root: String) -> SynSet?{
-        if synSets.count == 1 {
-            return synSets[0]
+    private func mostFrequent(literals: [Literal]) -> SynSet?{
+        if literals.count == 1 {
+            return self.turkishWordNet.getSynSetWithId(synSetId: literals[0].getSynSetId())
         }
         var minSense : Int = 50
         var best : SynSet? = nil
-        for synSet in synSets {
-            for i in 0..<synSet.getSynonym().literalSize() {
-                if Word.lowercase(s: synSet.getSynonym().getLiteral(index: i).getName()).hasPrefix(root)
-                    || Word.lowercase(s: synSet.getSynonym().getLiteral(index: i).getName()).hasSuffix(" " + root) {
-                    if synSet.getSynonym().getLiteral(index: i).getSense() < minSense {
-                        minSense = synSet.getSynonym().getLiteral(index: i).getSense()
-                        best = synSet
-                    }
-                }
+        for literal in literals {
+            if literal.getSense() < minSense {
+                minSense = literal.getSense()
+                best = self.turkishWordNet.getSynSetWithId(synSetId: literal.getSynSetId())
             }
         }
         return best
     }
     
     public override func autoLabelSingleSemantics(sentence: AnnotatedSentence) -> Bool{
+        var done : Bool = false
+        var twoPrevious : AnnotatedWord? = nil
+        var previous : AnnotatedWord? = nil
+        var twoNext : AnnotatedWord? = nil
+        var next : AnnotatedWord? = nil
         for i in 0..<sentence.wordCount() {
-            let synSets : [SynSet] = getCandidateSynSets(wordNet: turkishWordNet, fsm: fsm, sentence: sentence, index: i)
-            if synSets.count > 0 {
-                let best = mostFrequent(synSets: synSets, root: ((sentence.getWord(index: i) as? AnnotatedWord)?.getParse()!.getWord().getName())!)
-                if best != nil {
-                    (sentence.getWord(index: i) as? AnnotatedWord)!.setSemantic(semantic: best!.getId())
+            let current = sentence.getWord(index: i) as? AnnotatedWord
+            if i > 1 {
+                twoPrevious = sentence.getWord(index: i - 2) as? AnnotatedWord
+            }
+            if i > 0 {
+                previous = sentence.getWord(index: i - 1) as? AnnotatedWord
+            }
+            if i != sentence.wordCount() - 1 {
+                next = sentence.getWord(index: i + 1) as? AnnotatedWord
+            }
+            if i < sentence.wordCount() - 2 {
+                twoNext = sentence.getWord(index: i + 2) as? AnnotatedWord
+            }
+            if current!.getSemantic() == nil && current!.getParse() != nil {
+                if twoPrevious != nil && twoPrevious!.getParse() != nil && previous!.getParse() != nil {
+                    let literals : [Literal] = turkishWordNet.constructIdiomLiterals(morphologicalParse1: twoPrevious!.getParse()!, morphologicalParse2: previous!.getParse()!, morphologicalParse3: current!.getParse()!, metaParse1: twoPrevious!.getMetamorphicParse()!, metaParse2: previous!.getMetamorphicParse()!, metaParse3: current!.getMetamorphicParse()!, fsm: fsm)
+                    if literals.count > 0 {
+                        let bestSynSet = mostFrequent(literals: literals)
+                        if bestSynSet != nil{
+                            current!.setSemantic(semantic: (bestSynSet?.getId())!)
+                            done = true
+                            continue
+                        }
+                    }
+                }
+                if previous != nil && previous!.getParse() != nil && next != nil && next!.getParse() != nil {
+                    let literals : [Literal] = turkishWordNet.constructIdiomLiterals(morphologicalParse1: previous!.getParse()!, morphologicalParse2: current!.getParse()!, morphologicalParse3: next!.getParse()!, metaParse1: previous!.getMetamorphicParse()!, metaParse2: current!.getMetamorphicParse()!, metaParse3: next!.getMetamorphicParse()!, fsm: fsm)
+                    if literals.count > 0 {
+                        let bestSynSet = mostFrequent(literals: literals)
+                        if bestSynSet != nil{
+                            current!.setSemantic(semantic: (bestSynSet?.getId())!)
+                            done = true
+                            continue
+                        }
+                    }
+                }
+                if next != nil && next!.getParse() != nil && twoNext != nil && twoNext!.getParse() != nil {
+                    let literals : [Literal] = turkishWordNet.constructIdiomLiterals(morphologicalParse1: current!.getParse()!, morphologicalParse2: next!.getParse()!, morphologicalParse3: twoNext!.getParse()!, metaParse1: current!.getMetamorphicParse()!, metaParse2: next!.getMetamorphicParse()!, metaParse3: twoNext!.getMetamorphicParse()!, fsm: fsm)
+                    if literals.count > 0 {
+                        let bestSynSet = mostFrequent(literals: literals)
+                        if bestSynSet != nil{
+                            current!.setSemantic(semantic: (bestSynSet?.getId())!)
+                            done = true
+                            continue
+                        }
+                    }
+                }
+                if previous != nil && previous!.getParse() != nil {
+                    let literals : [Literal] = turkishWordNet.constructIdiomLiterals(morphologicalParse1: previous!.getParse()!, morphologicalParse2: current!.getParse()!, metaParse1: previous!.getMetamorphicParse()!, metaParse2: current!.getMetamorphicParse()!, fsm: fsm)
+                    if literals.count > 0 {
+                        let bestSynSet = mostFrequent(literals: literals)
+                        if bestSynSet != nil{
+                            current!.setSemantic(semantic: (bestSynSet?.getId())!)
+                            done = true
+                            continue
+                        }
+                    }
+                }
+                if current!.getSemantic() == nil && next != nil && next!.getParse() != nil {
+                    let literals : [Literal] = turkishWordNet.constructIdiomLiterals(morphologicalParse1: current!.getParse()!, morphologicalParse2: next!.getParse()!, metaParse1: current!.getMetamorphicParse()!, metaParse2: next!.getMetamorphicParse()!, fsm: fsm)
+                    if literals.count > 0 {
+                        let bestSynSet = mostFrequent(literals: literals)
+                        if bestSynSet != nil{
+                            current!.setSemantic(semantic: (bestSynSet?.getId())!)
+                            done = true
+                            continue
+                        }
+                    }
+                }
+                let literals : [Literal] = turkishWordNet.constructLiterals(word: current!.getParse()!.getWord().getName(), parse: current!.getParse()!, metaParse: current!.getMetamorphicParse()!, fsm: fsm)
+                if current!.getSemantic() == nil && literals.count > 0 {
+                    let bestSynSet = mostFrequent(literals: literals)
+                    if bestSynSet != nil{
+                        current!.setSemantic(semantic: (bestSynSet?.getId())!)
+                        done = true
+                        continue
+                    }
                 }
             }
         }
-        return true
+        return done
     }
 
 }
